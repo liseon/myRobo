656d1e0cc73848ed487a1dfdbb36b243d0bbe92f
Igor <ik.pr@bk.ru>
2013-12-01 23:54:34 +0400

    Fix critical!

--- module/Geometric.class.php
+++ module/Geometric.class.php
@@ -10,7 +10,7 @@ class Geometric
     public static function calcAngle($line1, $line2) {
         $ang1 = atan(($line1[1] - $line1[0])/1000);
         $ang2 = atan(($line2[1] - $line2[0])/1000);
-        $ang = $ang1 - $ang2;
+        $ang = abs($ang1 - $ang2);
 
         $ang = ($ang / pi()) * 180;
 
